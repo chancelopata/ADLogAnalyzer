db2670cc6186757e6917ff9773d804df01a1ffc4
Chance Lopata <chancelopata@Chances-MBP.ht.home>
2023-06-28 10:15:10 -0600

    All options work properly. abuseIPDB api call works and added --out to specify destination path/name.

--- ADLogAnalyzer.py
+++ ADLogAnalyzer.py
@@ -1,6 +1,6 @@
 '''ADLogAnalyzer
 Usage:
-    ADLogAnalyzer.py <logFilePath> [--ignoreIPs=<IP>... --ignoreUsers=<USER>... --countryWhitelist=<COUNTRYCODE>... (--abuseIPDB=<KEY> <THRESHOLD>)]
+    ADLogAnalyzer.py <logFilePath> [--ignoreIPs=<IP>... --ignoreUsers=<USER>... --countryWhitelist=<COUNTRYCODE>... (--abuseIPDB=<KEY> <THRESHOLD>) --out=<PATH>]
     ADLogAnalyzer.py -h | --help
     ADLogAnalyzer.py --version
 
@@ -9,7 +9,8 @@ Options:
     --ignoreIP: Ignore these IP addresses.
     --ignoreUser: Ignore these users
     --countryWhitelist: Treat all other countries as dangerous. Accepts 2 letter code.
-    --abuseIPDB <KEY> <THRESHOLD>: Key used in abuseIPDB lookup and the minimum number of failed sign ins from an IP required to launch a lookup.
+    --abuseIPDB <KEY> <THRESHOLD>: Key used in abuseIPDB lookup and the minimum number of failed sign ins from an IP required to launch a API request to abuseIPDB.
+    --out: Path for output.
 '''
 
 from docopt import docopt
@@ -22,13 +23,15 @@ pd.options.mode.chained_assignment = None
 
 VERSION = "1.0"
 
-# Performs API call for an IP against abuseipdb, returns full response in json
+# Performs API call for an IP against abuseipdb then returns the abuse confidence score, isp, and usage type.
 def checkAbuseIPDB(IP: str, apiKey: str) -> dict:
     r = requests.get(
         'https://api.abuseipdb.com/api/v2/check?ipAddress='+IP+'&maxAgeInDays=90&verbose',
         headers={'Key' : apiKey, 'Accept': 'application/json'}
     )
     r = r.json()['data']
+    r = r['abuseConfidenceScore'],r['isp'],r['usageType']
+    print(r)
     return r
 
 # Parse arguments from command
@@ -44,7 +47,8 @@ ignoreIPs = args['--ignoreIPs']
 ignoreUsers = args['--ignoreUsers']
 countryWhitelist = args['--countryWhitelist']
 abuseIPDBKey = args['--abuseIPDB']
-
+threshold = int(args['<THRESHOLD>'])
+out = args['--out']
 df = pd.read_csv(logFilePath)
 
 dangerousCountries = ['KR','KP','NK','CN','JP','RU']
@@ -57,7 +61,6 @@ dangerousCountries = ['KR','KP','NK','CN','JP','RU']
 if ignoreUsers:
     df = df[~df.User.isin(ignoreUsers)]
 if ignoreIPs:
-    print(10)
     df = df[~df['IP address'].isin(ignoreIPs)]
 
 # Normalize data
@@ -80,7 +83,15 @@ susFailedSignIns = df[['User','IP','Status']]
 
 # Get a list of users with failed sign ins and each IP they used.
 susFailedSignIns = susFailedSignIns[susFailedSignIns.Status == 'Failure']
-susFailedSignIns = susFailedSignIns.groupby(['User','IP']).count()
+
+if abuseIPDBKey is not None:
+    susFailedSignIns = susFailedSignIns.groupby(['User','IP']).count().reset_index()
+    susFailedSignIns['abuseConfidenceScore'], susFailedSignIns['isp'], susFailedSignIns['usageType'] = zip(*susFailedSignIns.apply(lambda x: ('','','') if x.Status < threshold else checkAbuseIPDB(x.IP,abuseIPDBKey),axis=1))
+    
+    # This makes the excel file easier to read. All it does is get rid of redundant names. Might want to sacrifice it for speed.
+    susFailedSignIns = susFailedSignIns.groupby(['User','IP','isp','abuseConfidenceScore','usageType']).sum() 
+else:
+    susFailedSignIns = susFailedSignIns.groupby(['User','IP']).count()
 
 # Get all sign ins from dangerous countries
 if  countryWhitelist:
@@ -90,13 +101,14 @@ else:
     dangerousCountrySignIns = df.loc[df['Country'].isin(dangerousCountries)]
     dangerousCountrySignIns = dangerousCountrySignIns.drop_duplicates()
 
-if abuseIPDBKey is not None:
-    pass
-
 #############################
 # Write df's to excel sheets
 #############################
-writer = pd.ExcelWriter(logFilePath + '_analyzed.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter
+if out:
+    writer = pd.ExcelWriter(out, engine='xlsxwriter')
+else:   
+    writer = pd.ExcelWriter(logFilePath + '_analyzed.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Filtered')
 dangerousCountrySignIns.to_excel(writer, sheet_name='DangerousCountry')
 susFailedSignIns.to_excel(writer, sheet_name='FailedSignIns')
